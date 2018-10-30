947b8ed51803ebc09258c24a092b744f29a139fc
Richard Garcia <richardgarciaph@gmail.com>
2018-10-31 11:42:02 +1300

    Remove cruft

---
+++ src/components/App/index.js
@@ -0,0 +1,21 @@
+// import React, { Component } from 'react'
+
+// class App extends Component {
+//   render () {
+//     return (
+//       <div className='App'>
+//         <header className='App-header'>
+//             Learn React
+//         </header>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
+
+import React from 'react'
+
+export default function App () {
+  return <h1>Tic-Tac-Toe</h1>
+}
